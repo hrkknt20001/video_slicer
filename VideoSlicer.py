02d4c5479294242947ed2f6ee90e7bde93b72397
hrkknt20001 <harutaka@pluto.dti.ne.jp>
2024-01-11 23:32:07 +0900

    2nd update

--- VideoSlicer.py
+++ VideoSlicer.py
@@ -3,6 +3,19 @@ import shutil
 import cv2
 import argparse
 
+def save_image(frame, angle, scale, dir, rule, idx ) :
+
+    height = frame.shape[0]
+    width = frame.shape[1]
+    center = (int(width / 2), int(height / 2))
+
+    # Geometric Image Transformations(use Affine Transform)
+    trans = cv2.getRotationMatrix2D(center, angle, scale)
+    frame = cv2.warpAffine(frame, trans, (width, height))
+
+    path = os.path.join(dir, rule % str(idx).zfill(6))
+    cv2.imwrite( path, frame)  # Save a frame
+    print('Save', path )
 
 def video_slicer(video_file, image_dir, image_file='img_%s.png',
                    frame_per_sec=6, frame_dist=100, start_idx=0, angle=0.0, scale=1.0):
@@ -31,12 +44,10 @@ def video_slicer(video_file, image_dir, image_file='img_%s.png',
     i = start_idx
     cap = cv2.VideoCapture(video_file)
 
-    if frame_dist is not None:
-        IMG_SIZE = (400, 400)
-        base_des = None
-        # detector = cv2.ORB_create()
-        detector = cv2.AKAZE_create()
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+    base_des = None
+    # detector = cv2.ORB_create()
+    detector = cv2.AKAZE_create()
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
     while cap.isOpened():
 
@@ -47,67 +58,47 @@ def video_slicer(video_file, image_dir, image_file='img_%s.png',
         if flag is False:
             break
 
-        height = frame.shape[0]
-        width = frame.shape[1]
-        frame = frame[0 : height, int((width - height)/2): int((width - height)/2) + height]
+        if frame_per_sec != -1 :
+            # Skip and sample the specified number of frames.
+            if cap.get(cv2.CAP_PROP_POS_FRAMES) % (frame_per_sec + 1) == 0:
+                if base_des is None :
+                    frame_resized = cv2.resize(frame, (400, 400))
+                    (base_kp, base_des) = detector.detectAndCompute(frame_resized, None)
+                    
+                    save_image(frame, angle, scale, image_dir, image_file, i )
 
-        if frame_dist is None and cap.get(cv2.CAP_PROP_POS_FRAMES) % frame_per_sec == 0:
-
-            height = frame.shape[0]
-            width = frame.shape[1]
-            center = (int(width / 2), int(height / 2))
-
-            # Geometric Image Transformations(use Affine Transform)
-            trans = cv2.getRotationMatrix2D(center, angle, scale)
-            frame = cv2.warpAffine(frame, trans, (width, height))
-
-            cv2.imwrite(image_dir+image_file % str(i).zfill(6), frame)  # Save a frame
-            print('Save', image_dir+image_file % str(i).zfill(6))
-            i += 1
-
-        else:
+                else:
+                    frame_resized = cv2.resize(frame, (400, 400))
+                    (comp_kp, comp_des) = detector.detectAndCompute(frame_resized, None)
+            
+                    matches = bf.match(base_des, comp_des)
+                    dist = [m.distance for m in matches]
+                    ret = sum(dist) / len(dist)
+                    print(f'dist:{ret}')
+                    if frame_dist < ret:
+                        save_image(frame, angle, scale, image_dir, image_file, i )
+                        base_des = comp_des
+        else :
+            # Skip similar frames.
             if base_des is None :
-                # Calculate feature point
-                frame_resized = cv2.resize(frame, IMG_SIZE)
+                frame_resized = cv2.resize(frame, (400, 400))
                 (base_kp, base_des) = detector.detectAndCompute(frame_resized, None)
 
-                height = frame.shape[0]
-                width = frame.shape[1]
-                center = (int(width / 2), int(height / 2))
-
-                # Geometric Image Transformations(use Affine Transform)
-                trans = cv2.getRotationMatrix2D(center, angle, scale)
-                frame = cv2.warpAffine(frame, trans, (width, height))
-
-                cv2.imwrite(image_dir+image_file % str(i).zfill(6), frame)  # Save a frame
-                print('Save', image_dir+image_file % str(i).zfill(6))
-                i += 1
+                save_image(frame, angle, scale, image_dir, image_file, i )
 
             else:
-                # Calculate feature point
-                frame_resized = cv2.resize(frame, IMG_SIZE)
+                frame_resized = cv2.resize(frame, (400, 400))
                 (comp_kp, comp_des) = detector.detectAndCompute(frame_resized, None)
         
-                # Finding distances from the results of feature matching
                 matches = bf.match(base_des, comp_des)
                 dist = [m.distance for m in matches]
                 ret = sum(dist) / len(dist)
-
+                print(f'dist:{ret}')
                 if frame_dist < ret:
-
-                    height = frame.shape[0]
-                    width = frame.shape[1]
-                    center = (int(width / 2), int(height / 2))
-
-                    # Geometric Image Transformations(use Affine Transform)
-                    trans = cv2.getRotationMatrix2D(center, angle, scale)
-                    frame = cv2.warpAffine(frame, trans, (width, height))
-
-                    cv2.imwrite(image_dir+image_file % str(i).zfill(6), frame)  # Save a frame
-                    print('Save', image_dir+image_file % str(i).zfill(6))
-
+                    save_image(frame, angle, scale, image_dir, image_file, i )
                     base_des = comp_des
-                    i += 1
+
+        i += 1
 
     cap.release()  # When everything done, release the capture
 
@@ -117,8 +108,8 @@ if __name__ == '__main__':
     parser.add_argument('arg1', help='input video file.')
     parser.add_argument('arg2', help='output directory.')
     parser.add_argument('--rule', help='naming rule of image file. ex) img_%s.png')
-    parser.add_argument('--fps', help='Number of frame per second.', type=int, default=6)
-    parser.add_argument('--dist', help='Distance of each frames.', type=int, default=None)
+    parser.add_argument('--skip_frame', help='Number of frames to skip, if -1, only the similarity between frames is valid.', type=int, default=0)
+    parser.add_argument('--skip_similarity', help='Similarity between frames to be skipped.', type=int, default=60)
     parser.add_argument('--start_index', help='start index of image file.', type=int, default=0)
     parser.add_argument('--angle', help='angle of rotate to image.', type=float, default=0.0)
     parser.add_argument('--scale', help='scale of image', type=float, default=1.0)
@@ -129,5 +120,5 @@ if __name__ == '__main__':
         args.arg3 = 'img_%s.png'
 
     video_slicer(video_file=args.arg1, image_dir=args.arg2,
-                 image_file=args.rule, frame_per_sec=args.fps, frame_dist=args.dist,
+                 image_file=args.rule, frame_per_sec=args.skip_frame, frame_dist=args.skip_similarity,
                  start_idx=args.start_index, angle=args.angle, scale=args.scale)
